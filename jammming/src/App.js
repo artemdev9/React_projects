97aafaf38819e773b60b52735c38caecac0e0240
Artem <dumchevartem@gmail.com>
2023-09-27 15:07:30 +0100

    c

---
+++ jammming/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import styles from './styles/App.module.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className={styles.logo}>
+        Jamz
+      </header>
+    </div>
+  );
+}
+
+export default App;
